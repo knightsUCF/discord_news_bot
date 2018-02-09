c52ec88b195fba393af24f2ec7064d25a8e50ada
Peter Kwiatkowski <peterk@knights.ucf.edu>
2018-02-09 06:31:28 -0500

    Update seven.py

--- seven.py
+++ seven.py
@@ -1,3 +1,67 @@
+
+import oandapyV20
+import oandapyV20.endpoints.accounts as accounts
+import oandapyV20.endpoints.orders as orders
+import oandapyV20.endpoints.trades as trades
+
+token = "dd6ef0ffad381d96bc6a589059095565-e3e957be473073523276c392d7b2453f"
+accountID = "101-001-1628549-002"
+
+client = oandapyV20.API(access_token = token)
+r = accounts.AccountDetails(accountID)
+client.request(r)
+
+
+
+
+
+
+
+def get_balance():
+    return (r.response['account']['balance'])
+
+
+def send_order(instrument, units):
+    data = {
+         "order": {
+            "units": units,
+            "instrument": instrument,
+            "timeInForce": "FOK",
+            "type": "MARKET",
+            "positionFill": "DEFAULT"
+          }
+    }
+    open_trade = orders.OrderCreate(accountID, data=data)
+    client.request(open_trade)
+    print (open_trade.response)
+
+
+def get_open_orders():
+    # params = {_v3_accounts_accountID_trades_params}
+    history = trades.TradesList(accountID=accountID) # params=params)
+    client.request(history)
+    print (history.response['trades'][0])
+
+
+
+# send_order('EUR_USD', '100')
+
+# get_balance()
+
+# get_open_orders()
+
+
+
+
+
+
+
+
+
+
+
+
+
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
@@ -18,6 +82,8 @@ client = commands.Bot(command_prefix = "!") #Initialise client bot
 
 username = "wizardsprite"
 password = "7haso6kSRiv4RL0cjK1-NChQ0-U"
+
+
 
 import praw
 import re
@@ -56,6 +122,12 @@ quotes = ['You are erratic. Conflicted. Disorganised. Every decision is debated,
           'The Borg believe otherwise.']
 
 
+
+output = str(get_balance())
+print(output)
+
+
+
 @client.event 
 async def on_ready():
     print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
@@ -76,7 +148,7 @@ async def on_message(message):
         command_one = '  !7"anything you want to ask" -- chat \n'
         command_four = '  !7of9 -- We are Borg\n'
         command_two = '  !7fxposts -- get the top ten posts from reddit / forex\n'
-        command_three = '  !7investingposts -- get the top ten posts from reddit / forex\n'
+        command_three = '  !7investingposts -- get the top ten posts from reddit / investing\n'
         output = output + command_one + command_two + command_three
         await client.send_message(message.channel, output)
     if message.content.startswith('!7'):
@@ -94,9 +166,12 @@ async def on_message(message):
         if message.content == "!7investingposts":
             output = 'Top 10 threads in https://www.reddit.com/r/investing : \n \n \n'
             number = 0
-            for submission in reddit.subreddit('forex').hot(limit=10):
+            for submission in reddit.subreddit('investing').hot(limit=10):
                 number += 1
                 output += (str(number) + '.' + ' ' + submission.title + '\n\n')
+            await client.send_message(message.channel, output)
+        if message.content == "!7balance":
+            output = str(get_balance())
             await client.send_message(message.channel, output)
     if message.content == "!7of9":
         output = str(random.choice(quotes))
@@ -105,3 +180,4 @@ async def on_message(message):
 
 
 client.run(real) #Replace token with your bots token
+
